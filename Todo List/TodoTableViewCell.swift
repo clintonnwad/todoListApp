81291ed7797b949ee7b0c6dbeabcd23e19c7ff02
Clinton Nwadiukor <113151586+clintonnwad@users.noreply.github.com>
2022-11-13 21:30:40 -0500

    More headers added

--- Todo List/TodoTableViewCell.swift
+++ Todo List/TodoTableViewCell.swift
@@ -4,6 +4,21 @@
 //
 //  Created by Jovi on 13/11/2022.
 //
+//  GROUP NUMBER: 15
+//  NAMES:
+//  Clinton Nwadiukor  - 301291242
+//  Jovi Chen-Mcintyre - 301125059
+//
+//  DESCRIPTION:
+//  Simple ToDo application that performs basic Todo functions
+//  Users can create a task, modify and update existing tasks
+//  The app is available in portrait and landscape
+//
+//  REVISION HISTORY:
+//  https://github.com/clintonnwad/todoListApp
+//
+//  DATE LAST MODIFIED:
+//  November 13, 2022
 
 import UIKit
 public protocol TodoItemCellDelegate:NSObjectProtocol {
